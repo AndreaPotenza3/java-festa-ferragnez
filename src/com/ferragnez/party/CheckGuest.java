844a21298b93b711194b43098ddd29c49dbfc3bb
AndreaPotenza <andreapoten3@gmail.com>
2025-04-02 13:08:05 +0200

    completo

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,54 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
-    String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+    public static void main(String[] args) {
+        
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual e' il tuo nome?");
+
+        String name = sc.nextLine().toLowerCase().trim();
+        
+        // for (int i = 0; i < guests.length; i++) {
+            
+        //     String listName = guests[i].toLowerCase().trim();
+
+        //     if(name.equals(listName)) {
+        //         System.out.println("Il tuo nome e' nella lista");
+
+        //         break;
+
+        //     } else {
+        //         System.out.println("Il tuo nome non e' nella lista");
+        //     }
+
+        // }
+
+        boolean isNameFound = false;
+        int i = 0;
+
+        while(!isNameFound && i < guests.length) {
+
+            String listName = guests[i].toLowerCase().trim();
+
+            if(name.equals(listName)) {
+                isNameFound = true;
+            }else {
+                i++;
+            }
+        }
+
+        if(isNameFound){
+            System.out.println("Il tuo nome e' nella lista");
+        } else {
+            System.out.println("Il tuo nome non e' nella lista");
+        }
+    }
+
 
 }
